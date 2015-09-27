ffabf4e03a519f175e6642225a68d9f64b03d160
mbernet79 <bernet.martin@gmail.com>
2015-09-27 17:52:02 +0200

    Update Test_candidateQSO.m

--- Test_candidateQSO.m
+++ Test_candidateQSO.m
@@ -5,7 +5,7 @@
 %Please check the readme.txt file for the applicability of the algorithm and its
 %restrictions
 %
-%Please cite Murphy, M.T., Bernet, M.L. ApJ,....(2015) if you use this code
+%Please cite Murphy, M.T., Bernet, M.L., 2015, MNRAS, submitted, if you use this code
 %Comments or reports of errors are welcome. Email: bernet.martin@gmail.com
 
 
